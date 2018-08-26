83ef4cbc9fa44b0d19e4c3cd3a143eb33bf39933
Daniel Saxton <daniel.saxton@gmail.com>
2018-08-26 09:58:34 -0500

    add tests and update __init__

--- resample/tests/test_utils.py
+++ resample/tests/test_utils.py
@@ -6,17 +6,18 @@ np.random.seed(2357)
 x = np.random.randn(100)
 f = ecdf(x)
 
+
 def test_ecdf_neg_inf():
     assert f(-np.inf) == 0.0
 
-def test_ecdf_pos_inf():
-    assert f(np.inf) == 1.0
 
 def test_ecdf_pos_inf():
     assert f(np.inf) == 1.0
+
 
 def test_mise_eq_func():
     assert mise(abs, abs, -3, 3, 7) == 0.0
+
 
 def test_sup_norm_eq_func():
     assert sup_norm(abs, abs, -3, 3, 7) == 0.0
